b644c2d50ad15d3f67c4189c19788e596df1517b
Jezer <sanchez.trigueros2027@hilasal.superate.org.sv>
2025-06-02 23:37:00 -0600

    Skybound Sneak V1.0

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ BLACK = (0, 0, 0)
 
 DIMENSIONS = (1375, 700)
 screen = pygame.display.set_mode((DIMENSIONS))
-pygame.display.set_caption("Skybound Sneak")
+pygame.display.set_caption("Skybound Sneak V1.0")
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(True)
